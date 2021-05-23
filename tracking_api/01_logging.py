a600d23d47edb66073c8b2572b3f65925288c11f
Camille Moatti <camille.moatti@gmail.com>
2021-05-23 19:43:28 +0000

    first model

--- tracking_api/01_logging.py
+++ tracking_api/01_logging.py
@@ -5,9 +5,9 @@ import mlflow
 import numpy as np
 import matplotlib.pyplot as plt
 
-mlflow.set_experiment("naive_experiment")
+mlflow.set_experiment("basic_experiment")
 with mlflow.start_run():
-    mlflow.log_param("my_parameter", .05)
+    mlflow.log_param("my_parameter", 0.05)
     mlflow.log_param("my_other_parameter", "Hello MLFlow")
     mlflow.log_metric("my_metric", 42)
     for i in range(100):
@@ -18,18 +18,18 @@ with mlflow.start_run():
     y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
     y2 = np.cos(2 * np.pi * x2)
     fig, (ax1, ax2) = plt.subplots(2, 1)
-    fig.suptitle('A tale of 2 subplots')
-    ax1.plot(x1, y1, 'o-')
-    ax1.set_ylabel('Damped oscillation')
-    ax2.plot(x2, y2, '.-')
-    ax2.set_xlabel('time (s)')
-    ax2.set_ylabel('Undamped')
+    fig.suptitle("A tale of 2 subplots")
+    ax1.plot(x1, y1, "o-")
+    ax1.set_ylabel("Damped oscillation")
+    ax2.plot(x2, y2, ".-")
+    ax2.set_xlabel("time (s)")
+    ax2.set_ylabel("Undamped")
     ############################
     mlflow.log_figure(fig, "my_plot.svg")
     # artifacts
     result = "This is an example artifact, an output from the run that should be kept"
     os.makedirs("results", exist_ok=True)
-    with open("results/result.txt", 'w') as f:
+    with open("results/result.txt", "w") as f:
         f.write(result)
 
     mlflow.log_artifact("results", "artifacts")
