968885d378703165a511f9ebeb4798c9e62c1e0a
Camille Moatti <camille.moatti@gmail.com>
2021-05-30 20:50:24 +0000

    work on aks

--- deploy_azure/07-1_deploy_model.py
+++ deploy_azure/07-1_deploy_model.py
@@ -1,6 +1,10 @@
+import sys
+
 from azureml.core import Workspace
 from azureml.core.model import Model
-from azureml.core.webservice import AciWebservice, Webservice
+from azureml.core.webservice import AciWebservice, Webservice, AksWebservice
+from azureml.core.compute import AksCompute, ComputeTarget
+from azureml.exceptions import ComputeTargetException
 
 import mlflow.azureml
 
@@ -18,17 +22,47 @@ workspace = Workspace.create(
     exist_ok=True,
 )
 
-model_name = "LinearRegressionModel"
-model_uri = f"models:/{model_name}/1"
+model_name = sys.argv[1]  # "LinearRegressionModel"
+model_uri = f"models:/{model_name}/{sys.argv[2]}"
 
-aci_service_name = "lr-uat-2"
-aci_service_config = AciWebservice.deploy_configuration(cpu_cores=1, memory_gb=1)
+env = "prod" if sys.argv[3].lower() == "prod" else "uat"
+
+service_name = f"lr-{env}"
+try:
+    Webservice(workspace, service_name).delete()
+except:
+    print("The service does not exist yet.")
+# Webservice.check_for_existing_webservice(workspace=workspace, name=service_name, check_func=Webservice.delete)
+service = AksWebservice if env == "prod" else AciWebservice
+service_config = service.deploy_configuration(cpu_cores=1, memory_gb=1)
+AksWebservice.deploy_configuration
+
+if env == "prod":
+    
+    aks_name = "aks-mlflow"
+
+    # Create the cluster
+    try:  
+        cpu_cluster = ComputeTarget(workspace=workspace, name=aks_name)  
+        print('Found existing cluster, use it.')  
+    except ComputeTargetException:
+        prov_config = AksCompute.provisioning_configuration(3,"Standard_DS2_v2")
+        aks_target = ComputeTarget.create(
+            workspace=workspace, name=aks_name, provisioning_configuration=prov_config
+        )
+        aks_target.wait_for_completion(show_output=True)
+        print(aks_target.provisioning_state)
+        print(aks_target.provisioning_errors)
+
+    service_config = service.deploy_configuration(
+        cpu_cores=1, memory_gb=1, compute_target_name=aks_name,
+    )
 
 azure_service, azure_model = mlflow.azureml.deploy(
     model_uri=model_uri,
-    service_name=aci_service_name,
+    service_name=service_name,
     model_name=model_name,
     workspace=workspace,
-    deployment_config=aci_service_config,
+    deployment_config=service_config,
     synchronous=True,
 )
